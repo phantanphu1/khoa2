7bdf315751fd8f43d58d779747442fc1d6c3cc79
phanhungphong <phanhungphong.qn@gmail.com>
2021-08-19 22:22:36 +0700

    bai

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
+import Con from './connho';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('phu');
+  const [grade, setGrade] = useState(1);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +19,23 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
+
       </header>
+      <div className="moi">
+        <Con myname ={name}/>
+        <Con myname ={'phan tấn phú'}/>
+        <p>tôi tên la {name}</p>
+        <p>toi hoc lop {grade}</p>
+        <button onClick={()=>setName('Tấn phú')}>đỏi tên</button>
+        <button onClick={()=>setGrade(grade +1)}>đổi lớp</button>
+        <h3>phu</h3>
+        <img src="https://www.takadada.com/wp-content/uploads/2019/07/9-16.jpg"></img>
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/connho.js
@@ -0,0 +1,10 @@
+import React from'react';
+import './connho.css';
+
+function Con(props) {
+  return (
+   <h1>tôi tên là {props .myname}</h1>
+  );
+}
+
+export default Con;
